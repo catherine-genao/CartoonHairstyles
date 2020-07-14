7562c0f9163ba2abae2f90e26fd741233d56bff3
catherine genao <cgenao80@gmail.com>
2020-07-14 15:39:19 -0400

    update

--- FinalProject/app/src/main/java/com/example/finalproject/AdventureTimeQuiz.java
+++ FinalProject/app/src/main/java/com/example/finalproject/AdventureTimeQuiz.java
@@ -71,40 +71,40 @@ ArrayList <Question> answers;
         HashMap<String, String> question3 = new HashMap<String, String>();
     question3.put("question", "You go to the beach and your friend forgets sunscreen. What do you do?");
     question3.put("I would most likely come with extra sunscreen", "Princess Bubblegum");
-    question3.put("Oh well, that’s not my problem.", "Marceline");
-    question3.put("I wasn’t invited so… ", "the Ice king");
-    question3.put("Try to buy some new sunscreen.", "finn");
-    question3.put("Borrow someone’s sunscreen & give it back after.", "jake");
+    question3.put("Oh well, that’s not my problem", "Marceline");
+    question3.put("I wasn’t invited so…", "the Ice king");
+    question3.put("Try to buy some new sunscreen", "finn");
+    question3.put("Borrow someone’s sunscreen & give it back", "jake");
     questions.add(question3);
 
     HashMap<String, String> question4 = new HashMap<String, String>();
     question4.put("question", "How would you plan a party?");
-    question4.put("Responsible, Kind", "Princess Bubblegum");
-    question4.put("Down to earth,chill", "Marceline");
-    question4.put("Uptight, Short tempered", "the Ice king");
-    question4.put("Adventurous, Helpful, Loving", "finn");
-    question4.put("funny, tough, loveable", "jake");
+    question4.put("A surprise party!", "Princess Bubblegum");
+    question4.put("Hangout session with a few friends in the woods", "Marceline");
+    question4.put("UGet them a gift & bring a lot of friends", "the Ice king");
+    question4.put("Make a fun activity", "finn");
+    question4.put("Small get together, I cook", "jake");
     questions.add(question4);
 
     HashMap<String, String> question5 = new HashMap<String, String>();
-    question5.put("question", "What would people describe you as?");
-    question5.put("Responsible, Kind", "Princess Bubblegum");
-    question5.put("Down to earth,chill", "Marceline");
-    question5.put("Uptight, Short tempered", "the Ice king");
-    question5.put("Adventurous, Helpful, Loving", "finn");
-    question5.put("funny, tough, loveable", "jake");
+    question5.put("question", "If you knew you had a few days left to live, what would you do?");
+    question5.put("Try to reach your goals in life.", "Princess Bubblegum");
+    question5.put("Get some alone time.", "Marceline");
+    question5.put("Let everyone know", "the Ice king");
+    question5.put("Help people as much as possible", "finn");
+    question5.put("Visit my family members", "jake");
     questions.add(question5);
 
 
 
 
     q1 = new Question(" What do you want to be in the future?","Pediatrician","Musician","The President", "Hero","Comedian");
-       q2 = new Question(" What would people describe you as?","Responsible, Kind","Down to earth,chill","Uptight, Short tempered", "Adventurous, Helpful, Loving","funny, tough, loveable");
-       q3 = new Question("You go to the beach and your friend forgets sunscreen. What do you do?","I would most likely come with extra sunscreen","Oh well, that’s not my problem.","I wasn’t invited so…","Try to buy some new sunscreen.","Borrow someone’s sunscreen & give it back after.");
-    q4 = new Question("How would you plan a party?","A surprise party!","Hangout session with a few friends in the woods.","Get them a personalized gift & bring a lot of friends.","Make a fun activity for the people who are invited!","Small get together, I cook.");
+    q2 = new Question(" What would people describe you as?","Responsible, Kind","Down to earth,chill","Uptight, Short tempered", "Adventurous, Helpful, Loving","funny, tough, loveable");
+    q3 = new Question("You go to the beach and your friend forgets sunscreen. What do you do?","I would most likely come with extra sunscreen","Oh well, that’s not my problem","I wasn’t invited so…","Try to buy some new sunscreen","Borrow someone’s sunscreen & give it back");
+    q4 = new Question("How would you plan a party?","A surprise party!","Hangout session with a few friends in the woods","Get them a gift & bring a lot of friends","Make a fun activity","Small get together, I cook");
+    q5 = new Question("If you knew you had a few days left to live, what would you do?","Try to reach your goals in life. ","Get some alone time.","Let everyone know","Help people as much as possible","Visit my family members");
 
-       ;
-      //  Question q5 = new Question("If you knew you had a few days left to live, what would you do?");
+
 
 answers.add(q1);
 answers.add(q2);
@@ -121,16 +121,22 @@ answers.add(q5);
     a5.setText(answers.get(counter).getAnswer5()) ;
 }
 public void checkanswer(String choice){
-String character = questions.get(counter).get(choice).toString();
+String character;
+    if(counter < answers.size())
+        character = questions.get(counter).get(choice).toString();
 }
 
     public void nextquestion(View view){
         Button btn = (Button)view;
-        String buttonText = btn.getText().toString();
-    checkanswer(buttonText);
 
-counter++;
-        if (answers.get(counter).getAnswer1()== null){
+        if (counter<answers.size()){
+            String buttonText = btn.getText().toString();
+            Log.d("buttonINfo", buttonText);
+            checkanswer(buttonText);
+             counter++;
+        }
+
+        if (counter >= answers.size()-1){
             Toast.makeText(this , "You reach the end of the test",Toast.LENGTH_LONG);
         }
 else {
